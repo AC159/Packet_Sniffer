555fde75711d81662fdabc7b98077b30a2d6bd2c
Anastassy Cap <anacap100@gmail.com>
2021-01-01 21:23:43 -0500

    Added more functionality

--- main.py
+++ main.py
@@ -1,9 +1,50 @@
 from scapy.all import *
 from scapy.layers.inet import IP, ICMP
 from chalky import sty, fg, bg
+import argparse
+from banners import print_banner
+import sys
+import netifaces
 
 
 def main():
+
+    print_banner()
+
+    parser = argparse.ArgumentParser(description="Packet Sniffer")
+    group = parser.add_mutually_exclusive_group()
+
+    # Can't have all these options turned on:
+    group.add_argument('--sniff_basic', action='store_true', help="Give basic information about each sniffed packet")
+    group.add_argument('--sniff_detail', action='store_true', help="Give detailed information about each sniffed packet")
+    group.add_argument('-p', '--ping', type=str, help="Ping domain name or IP address")
+    group.add_argument('--interfaces', action='store_true', help="Show a list of detected network interfaces")
+
+    parser.add_argument('-i', '--interface', type=str, help="Name of the wireless network adapter")
+    parser.add_argument('-c', '--count', type=int, help='# of packets to sniff')
+
+    args = parser.parse_args()
+
+    if args.interfaces:
+        print(netifaces.interfaces())
+
+    # todo: determine why the code runs (sniff_basic && sniff_detail) even if they are not specified, maybe
+    #  because of the store_true? ... but it's supposed to store False if not specified...
+
+    if args.sniff_basic:
+
+        if args.count:
+            sniff(count=args.count, iface="wlx00c0caaba31a", prn=lambda p: p.summary())
+        else:
+            # Display a basic summary of each captured packet:
+            sniff(iface="wlx00c0caaba31a", prn=lambda p: p.summary())
+
+    if args.sniff_detail:
+        if args.count:
+            sniff(count=args.count, iface="wlx00c0caaba31a", prn=lambda p: p.show())
+        else:
+            # Display a basic summary of each captured packet:
+            sniff(iface="wlx00c0caaba31a", prn=lambda p: p.show())
 
     debug = sty.dim & fg.white
     success = fg.bright_blue & sty.bold
@@ -19,9 +60,6 @@ def main():
     #
     # print(success | "Here are some details about the packets: ")
     # print(packets.summary())
-
-    # Display a basic summary of each captured packet:
-    sniff(iface="wlx00c0caaba31a", prn=lambda p: p.summary())
 
     # Display detailed information about each packet:
     sniff(iface="wlx00c0caaba31a", prn=lambda p: p.show())
---
+++ main_gui.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+
+def main():
+
+    window = Tk()
+    window.title("Packet Sniffer")
+
+    window.mainloop()
+
+
+if __name__ == "__main__":
+    main()
